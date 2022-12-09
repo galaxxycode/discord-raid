007a01b6c0721d60052995173a26d9474fbcd545
galaxxycode <120145136+galaxxycode@users.noreply.github.com>
2022-12-08 18:33:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
 import discord
 from discord.ext import commands
-
 
 client = commands.Bot(command_prefix=".",  status=discord.Status.idle, activity = discord.Activity(type=discord.ActivityType.watching, name=";)"), intents=discord.Intents.all())
 
