0c90eb8a23c36e88190f6785036dc2ea39d41aae
galaxxycode <120145136+galaxxycode@users.noreply.github.com>
2022-12-08 18:29:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ async def on_ready():
 
 @client.command()
 async def l(ctx):
+  await ctx.channel.purge(limit=1)
   await ctx.send('Someone you know has requested for this bot to raid your server, have fun')
   await ctx.send('Raided')
   await ctx.send('Raided')
